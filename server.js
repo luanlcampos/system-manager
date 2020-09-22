5775eb6c1cbd4ac71308bfe7f6c372175215d46c
Luan Campos <luanlcampos15@gmail.com>
2020-09-21 20:52:45 -0400

    delete test function

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ console.log("Express http server listening on port " + HTTP_PORT);
 
 app.use(express.static("public"))
 
-app.get("/test", function(req, res){
-    res.send("Testing it")
-})
 //sending the home page
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "views/home.html"))
