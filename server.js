cf80a3e173245af932de212e5931181bf1495a88
Luan Campos <luanlcampos15@gmail.com>
2020-09-21 20:45:28 -0400

    defined home and about routes

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+var HTTP_PORT = process.env.HTTP_PORT || 8080;
+
+console.log("Express http server listening on port " + HTTP_PORT);
+
+app.use(express.static("public"))
+
+app.get("/test", function(req, res){
+    res.send("Testing it")
+})
+//sending the home page
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "views/home.html"))
+})
+
+//sending the about page
+app.get("/about", function(req, res){
+    res.sendFile(path.join(__dirname, "views/about.html"))
+})
+
